49dad1a57a4020ad5106a430e143c5c56a96d2ee
Roshan <shahroshan0720@gmail.com>
2023-08-13 20:27:38 +0530

    frontend connected

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 app.use(express.static('public'));
-/*
-app.get('*', (req, res) => {
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'/public/index.html'));
-});*/
+});
 
 app.use('/api/v1/transactions', transcations);
 
---
+++ public/script.js
@@ -0,0 +1,105 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+let transID;
+
+// Add transaction
+function addTransaction(e) {
+    e.preventDefault();
+
+    if (text.value.trim() === '' || amount.value.trim() === '') {
+        alert('Please add a text and amount');
+    } else {
+        axios.post('http://localhost:4005/api/v1/transactions', {
+            text: text.value,
+            amount: amount.value
+        }).then(resp => {
+            addTransactionDOM(resp.data.data);
+            updateValues();
+
+            text.value = '';
+            amount.value = '';
+        }).catch ((err) => {
+            console.log(err);
+        })
+    }
+}
+
+// Add transactions to DOM list
+function addTransactionDOM(transaction) {
+    // Get sign
+    const sign = transaction.amount < 0 ? '-' : '+';
+
+    const item = document.createElement('li');
+    transID = transaction._id;
+    // Add class based on value
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+    item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> 
+    <button class="delete-btn" data-id="${transaction._id}">x</button>
+    `;
+    list.appendChild(item);
+
+    const deleteTrans = document.querySelectorAll('.delete-btn');
+    deleteTrans.forEach((item)=>{
+        item.addEventListener('click', (e) => {
+            removeTransaction(e.currentTarget.dataset.id)
+        });
+    })
+}
+
+// Update the balance, income and expense
+function updateValues() {
+
+    axios.get('http://localhost:4005/api/v1/transactions', {
+        timeout: 5000
+    }).then(resp => {
+        let transactions = resp.data.data;
+        const amounts = transactions.map(transaction => transaction.amount);
+
+        const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+        const income = amounts
+            .filter(item => item > 0)
+            .reduce((acc, item) => (acc += item), 0)
+            .toFixed(2);
+
+        const expense = (
+            amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+            -1
+        ).toFixed(2);
+
+        balance.innerText = `$${total}`;
+        money_plus.innerText = `$${income}`;
+        money_minus.innerText = `$${expense}`;
+    }).catch((err) => {
+        console.log(err);
+    })
+}
+
+// Remove transaction by ID
+function removeTransaction(id) {
+    axios.delete(`http://localhost:4005/api/v1/transactions/${id}`)
+    .then(resp=>{
+        if (resp.data.success) {
+            updateValues();
+        }
+    }).catch((err)=>{
+        console.log(err);
+    })
+}
+
+// Init app
+async function init() {
+    list.innerHTML = '';
+    updateValues();
+}
+
+init();
+form.addEventListener('submit', addTransaction);
